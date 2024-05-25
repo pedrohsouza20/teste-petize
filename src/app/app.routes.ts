ca9ef327aef0d2a9c822974338f85fd6dc30df8a
Pedro <contato.pedromoc@gmail.com>
2024-05-25 17:20:14 -0300

    feat: desenvolvida a base do roteamento do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        pathMatch: 'full'
+    },
+
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+];
